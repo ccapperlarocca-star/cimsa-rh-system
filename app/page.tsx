6029852aa63957eed8ffa10a88a1f5cea2b0f403
José Aguilera <ccapperlarocca@gmail.com>
2026-05-21 18:05:16 -0600

    fix: estado candidatos faltante

--- app/page.tsx
+++ app/page.tsx
@@ -87,6 +87,9 @@ export default function Home() {
     setVacanteIdSeleccionada,
   ] = useState("");
 
+  const [candidatos, setCandidatos] =
+    useState<any[]>([]);
+
   // =====================================================
   // STATES VACANTES
   // =====================================================
