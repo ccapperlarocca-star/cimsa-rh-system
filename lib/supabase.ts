b2f2a3dceaf824d4bb2f72ccf0a5aa2fa47dce42
José Aguilera <ccapperlarocca@gmail.com>
2026-08-10 16:22:11 -0600

    Mover configuración de Supabase a variables de entorno

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://zqsxcpjwycxmbaocmwml.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpxc3hjcGp3eWN4bWJhb2Ntd21sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzMDYwOTEsImV4cCI6MjA5NDg4MjA5MX0.Q8TLS9HAFx1Rn1B4hsWmf9VuhyZc0DKpA1hXM6CNQWY";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(
   supabaseUrl,
